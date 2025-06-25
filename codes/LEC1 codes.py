2b6b2ae35ed522001b3af8d057dabf416dd1f482
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-06-25 23:37:08 +0530

    Update LEC1 codes.py

--- codes/LEC1 codes.py
+++ codes/LEC1 codes.py
@@ -8,8 +8,26 @@ print(prev1)
 for fibo in range (18):
     newfibo = prev1 + prev2
     print(newfibo)
-    prev2 = prev1
-    prev1 = newfibo
+    prev2 = prev1    # To assign a new previous value as it is sum of last 2 numbers
+    prev1 = newfibo  # here prev1 got new value and we already assigned prev1 value to prev1 in last line
 
 
-#
+#implemention using recursion
+
+print(0)
+print(1)
+count = 2
+
+def fibonacci (prev1, prev2):
+    global count
+    if count < 19:
+        newfibo = prev1 + prev2
+        print(newfibo)
+        prev2 = prev1
+        prev1 = newfibo
+        count += 1
+        fibonacci(prev1 , prev2)
+    else:
+        return
+
+    fibonacci(1,0)
