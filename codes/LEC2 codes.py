b90292a5145854e9535973742d1f233be88794a3
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-06-29 02:17:56 +0530

    Update LEC2 codes.py

--- codes/LEC2 codes.py
+++ codes/LEC2 codes.py
@@ -9,3 +9,21 @@ print (my_array [0])
 
 
 
+# how algorithm is written
+
+#This is Pseudocode
+# minVal = current element
+#Variable 'minVal' = array[0]
+#For each element in the array
+    #If current element < minVal
+
+# code of sorting this algorithm
+my_array = [7, 12, 9, 4, 11]
+minValue = my_array[0]
+
+for i in my_array:
+    if i < minValue:
+        minValue = i
+
+print ('lowest Value', minValue)
+
