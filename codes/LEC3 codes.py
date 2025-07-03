601f8942980530356ec6eb42dc98ede7958120ba
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-07-04 00:14:57 +0530

    Update LEC3 codes.py

--- codes/LEC3 codes.py
+++ codes/LEC3 codes.py
@@ -1,3 +1,4 @@
+'''
 my_array = [15,12,5,85,54,95,25,3,]
 
 n = len(my_array)
@@ -7,6 +8,25 @@ for i in range (n-1):
             my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
 
 print("Sorted array is :", my_array)
+'''
+# Improved Bubble Sort
+
+my_array = [8,5,6,9,15,12]
+
+n= len(my_array)
+for i in range (n-1):
+    swapped = False
+    for j in range (n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j] ,my_array[j+1] = my_array[j+1] , my_array[j]
+            swapped = True
+        if not swapped:
+            break
+
+print("Sorted array is :", my_array)
+
+
+
 
 
 
