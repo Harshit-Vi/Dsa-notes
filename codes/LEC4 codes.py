c384e368f06f4af019b22c897676efcd9ac322dd
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-07-06 02:41:42 +0530

    Update LEC4 codes.py

--- codes/LEC4 codes.py
+++ codes/LEC4 codes.py
@@ -1,3 +1,4 @@
+'''
 my_array = [64, 34, 25, 5, 22, 11, 90, 12]
 
 n = len(my_array)
@@ -10,3 +11,19 @@ for i in range (n-1):
     my_array.insert(i , min_value)
 
 print("Sorted array is :", my_array)
+'''
+# Improved selecetion sort by swapping method
+
+my_array = [64, 34, 25, 5, 22, 11, 90, 12]
+
+n= len(my_array)
+for i in range (n-1):
+    min_index = i
+    for j in range (i+1 , n):
+        if  my_array[j] < my_array[min_index]:
+            min_index = j
+    my_array[i], my_array[min_index] = my_array[min_index], my_array[i]
+
+print("Sorted array is :", my_array)
+
+
