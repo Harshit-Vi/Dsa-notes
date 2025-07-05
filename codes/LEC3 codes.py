84f853c5951ee375d3973625c17276fc4fc9b43f
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-07-05 23:49:29 +0530

    Update LEC3 codes.py

--- codes/LEC3 codes.py
+++ codes/LEC3 codes.py
@@ -13,12 +13,12 @@ print("Sorted array is :", my_array)
 
 my_array = [8,5,6,9,15,12]
 
-n= len(my_array)
+n = len(my_array)
 for i in range (n-1):
     swapped = False
     for j in range (n-i-1):
         if my_array[j] > my_array[j+1]:
-            my_array[j] ,my_array[j+1] = my_array[j+1] , my_array[j]
+            my_array[j] ,my_array[j+1] = my_array[j+1], my_array[j]
             swapped = True
         if not swapped:
             break
