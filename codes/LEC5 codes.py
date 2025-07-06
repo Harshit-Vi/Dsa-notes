bab4ee33b08a9cfa5031f6234bb231b013904c57
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-07-07 00:22:37 +0530

    Update LEC5 codes.py

--- codes/LEC5 codes.py
+++ codes/LEC5 codes.py
@@ -1,3 +1,4 @@
+'''
 my_array =[64, 34, 25, 12, 22, 11, 90, 5]
 
 n = len(my_array)
@@ -10,4 +11,18 @@ for i in range(1,n):
     my_array.insert(insert_index , current_value)
 
 print("Sorted array is :", my_array)
+'''
 
+# Improved code
+
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n= len(my_array)
+for i in range(1,n):
+    insert_index = i
+    current_value = my_array[i]
+    for j in range(i-1,-1,-1):
+        if my_array[j] > current_value:
+            insert_index = j
+            my_array[j+1] = my_array[j]
+            
